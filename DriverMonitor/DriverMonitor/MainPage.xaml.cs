1d6243b6b832cb2eba89c6413cf906b38b983286
CutCutto <sittinut.k@cdssolution.com>
2017-07-04 14:39:07 +0700

    คอมมิทครั้งแรก

---
+++ DriverMonitor/DriverMonitor/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DriverMonitor
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       async  private void bntRegister_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Register());
+        }
+
+        async private void btnSignIn_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Login());
+        }
+    }
+}
